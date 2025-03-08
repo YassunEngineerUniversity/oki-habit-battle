140fbd13132aa4b4441e1554bd9aefd1c44a7486
kei-1313 <candydog666@gmail.com>
2025-03-08 13:20:40 +0900

    add: createアクションの作成

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -5,6 +5,17 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-    
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      render :create
+    else
+      render_400(user.errors.full_messages)
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
 end
