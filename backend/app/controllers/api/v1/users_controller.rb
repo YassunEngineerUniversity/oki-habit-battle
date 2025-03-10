356603e56bae321e56ff3361d0714fce1c18c21c
kei-1313 <candydog666@gmail.com>
2025-03-10 13:43:33 +0900

    fix: ブロック節を追加し、早期returnさせる

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -3,40 +3,26 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     user = User.new(create_user_params)
-    if user.save
-      session[:user_id] = user.id
-      render :create
-    else
-      render_400(user.errors.full_messages)
-    end
+    return render_400(user.errors.full_messages) unless user.save
+
+    session[:user_id] = user.id
+    render :create
   end
 
   # 使用想定画面: プロフィール画面、プロフィール更新画面
   def me
     @current_user = current_user
-    if @current_user
-      render :me
-    else
-      render_404("ユーザが見つかりませんでした")
-    end
   end
 
   def update
-    if current_user.update(update_user_params)
-      render :update
-    else
-      render_400(current_user.errors.full_messages)
-    end
+    render_400(current_user.errors.full_messages) unless current_user.update(update_user_params)
   end
 
   def destroy
-    if current_user.update_column(:deleted_at, Time.zone.now)
-      reset_session
-      cookies.delete(:_habitbattle_session)
-      render :destroy
-    else
-      render_422("退会処理中にエラーが発生しました")
-    end
+    return render_422("退会処理中にエラーが発生しました") unless current_user.update_column(:deleted_at, Time.zone.now)
+
+    reset_session
+    cookies.delete(:_habitbattle_session)
 
     # Todo: 一定期間経つと削除する処理を追加
   end
