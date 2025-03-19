554dae5b93b90ee6721c8149032ef1333de8968e
kei-1313 <candydog666@gmail.com>
2025-03-20 00:51:49 +0900

    add: カテゴリーの一覧APIのリクエストテストを作成

--- backend/spec/requests/api/v1/categories/index_spec.rb
+++ backend/spec/requests/api/v1/categories/index_spec.rb
@@ -1,26 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe "users_controller me", type: :request do
+RSpec.describe "categories_controller index", type: :request do
   let!(:user) { FactoryBot.create(:user) }
+  let!(:categories) { FactoryBot.create_list(:category, 10) }
   let(:json_response) { JSON.parse(response.body) }
 
-  subject { get "/api/v1/me" }
+  subject { get "/api/v1/battle-categories" }
 
   shared_examples "Successful case" do | status |
-    it "ログインしているユーザが取得できる" do
+    it "カテゴリー一覧を取得できる" do
       subject
       expect(response).to have_http_status(status)
-      expect(json_response["id"]).to eq(user.id)
-      expect(json_response["name"]).to eq(user.name)
-      expect(json_response["email"]).to eq(user.email)
-      expect(json_response["profile"]).to eq(user.profile)
-      expect(json_response["image_url"]).to eq(user.image_url)
-      expect(json_response["reword_total"]).to eq(user.reword_total)
+      battle_categories = Category.all
+      expect(json_response.length).to eq(battle_categories.length)
+
+      json_response.each_with_index do |category, index|
+        expect(category["id"]).to eq(battle_categories[index].id)
+        expect(category["name"]).to eq(battle_categories[index].name)
+        expect(category["image_url"]).to eq(battle_categories[index].image_url)
+      end
     end
   end
 
   shared_examples "Error case" do | status, error_message |
-    it "ユーザが取得できない" do
+    it "カテゴリー一覧を取得できない" do
       subject
       expect(response).to have_http_status(status)
       expect(json_response["errors"]).to eq(error_message)
@@ -32,7 +35,8 @@ RSpec.describe "users_controller me", type: :request do
       post "/api/v1/login", params: { email: user.email, password: user.password }
     end
 
-    context "正常なログインが存在する場合" do
+    # 正常系
+    context "正常にカテゴリー一覧が取得できる場合" do
       include_examples "Successful case", :ok
     end
   end
