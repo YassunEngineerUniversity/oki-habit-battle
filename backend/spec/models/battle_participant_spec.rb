13dcc8f5b45091cd66e77e3cb6af91778592d5e6
kei-1313 <candydog666@gmail.com>
2025-03-19 22:15:16 +0900

    add: battle_participant モデルのテストを作成

--- backend/spec/models/battle_participant_spec.rb
+++ backend/spec/models/battle_participant_spec.rb
@@ -21,5 +21,18 @@
 require 'rails_helper'
 
 RSpec.describe BattleParticipant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { FactoryBot.create(:user) }
+  let!(:other_user) { FactoryBot.create(:user, :with_battles) }
+  let!(:battle_participant) { FactoryBot.build(:battle_participant, user: user, battle: other_user.battles.sample) }
+
+  # 正常系
+  context "正常にバトル参加者が作成される場合" do
+    it "BattleParticipantテーブルに保存される" do
+      battle_participant.save
+      expect(battle_participant).to be_valid
+
+      saved_battle_participant = BattleParticipant.find_by(user_id: battle_participant.user_id, battle_id: battle_participant.battle_id)
+      expect(saved_battle_participant).to be_present
+    end
+  end
 end
