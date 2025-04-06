9d687c932d9a4798baa6705196877ffc6ccf1079
kei-1313 <candydog666@gmail.com>
2025-04-06 16:41:54 +0900

    refactor: indexアクションのバトル取得ロジックを改善
    - current_user.battlesを使用していた部分を、Battleモデルからbattle_participantsを結合して取得する形に変更しました。

--- backend/app/controllers/api/v1/homes_controller.rb
+++ backend/app/controllers/api/v1/homes_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::HomesController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @active_battles = current_user.battles.joins(:battle_history).preload(:battle_history).where(battle_histories: { status: Status::ACTIVE })
-    @waiting_battles = current_user.battles.joins(:battle_history).preload(:battle_history).where(battle_histories: { status: Status::WAITING })
+    battles = Battle.joins(:battle_participants).preload(:battle_participants).where(battle_participants: { user_id: current_user.id })
+    @active_battles = battles.joins(:battle_history).preload(:battle_history).where(battle_histories: { status: Status::ACTIVE })
+    @waiting_battles = battles.joins(:battle_history).preload(:battle_history).where(battle_histories: { status: Status::WAITING })
   end
 end
