ee57ff65a7e579e368f5dcb02597efd65add8e7e
kei-1313 <candydog666@gmail.com>
2025-04-29 16:58:52 +0900

    add: 対戦の履歴に達成と未達成用の振り分けするため、achievement_statusカラムを追加

--- backend/spec/factories/battle_histories.rb
+++ backend/spec/factories/battle_histories.rb
@@ -2,11 +2,12 @@
 #
 # Table name: battle_histories
 #
-#  id         :bigint           not null, primary key
-#  status     :string(255)      default("waiting"), not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  battle_id  :bigint           not null
+#  id                 :bigint           not null, primary key
+#  achievement_status :boolean          default(FALSE), not null
+#  status             :string(255)      default("waiting"), not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  battle_id          :bigint           not null
 #
 # Indexes
 #
